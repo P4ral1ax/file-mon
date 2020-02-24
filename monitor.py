11388804ce1846a8933f288fc27894abfaf2220e
PapaChain <braydenjw00@gmail.com>
2020-02-24 18:09:08 -0500

    sike

--- monitor.py
+++ monitor.py
@@ -8,25 +8,16 @@ from create_dataclass import *
 After the UI is collected and all the files are set to be monitored, this
 is where the program will do the monitoring.
 """
-def path_exists(path):
-    return path.exists(path)
-
-
 def check_dir(files, directories, index):
     dir_changed = False
     dir = directories[index]
     log = open('log.txt', 'a')
     info = os.stat(dir.path)
 
-    if not path_exists(dir.path):
-        dir.name == 'DEL'
-
-
-
     #Dir Permissions
     if info.st_mode != dir.perm:
-        print("Permissions : " + dir.name + " | time : " + time.strftime('%H:%M:%S') +
-         "\n    Old Perm: " + str(oct(dir.perm)) + "\n    New Perm: " + str(oct(info.st_mode)), file=log, end='\n')
+        print("Permissions : " + dir.name + " | time : " + time.strftime('%H:%M:%S') + "\n    Old Perm: " \
+        + str(oct(dir.perm)) + "\n    New Perm: " + str(oct(info.st_mode)), file=log, end='\n')
         dir_changed = True
     #Dir Access Time
     if info.st_atime != dir.time_acc:
@@ -56,18 +47,15 @@ def check_file(files, directories, index):
     log = open('log.txt', 'a')
     info = os.stat(file.path)
 
-
-    if file.name == 'DEL':
-        pass
     #File Size
     if info.st_size != file.size:
-        print("Size Changed : " + file.name + " | time : " +  time.strftime('%H:%M:%S') + \
-        "\n    Old Size: " + str(file.size) + "B\n    New Size: " + str(info.st_size) + "B", file=log, end='\n')
+        print("Size Changed : " + file.name + " | time : " +  time.strftime('%H:%M:%S') + "\n    Old Size:  " \
+        + str(file.size) + "B\n    New Size: " + str(info.st_size) + "B", file=log, end='\n')
         file_changed = True
     #File Permissions
     if info.st_mode != file.perm:
-        print("Permissions Changed: " + file.name + " | time : " + time.strftime('%H:%M:%S') + "\n    Old Perm: " + \
-        str(oct(file.perm)) + "\n    New Perm: " + str(oct(info.st_mode)), file=log, end='\n')
+        print("Permissions Changed: " + file.name + " | time : " + time.strftime('%H:%M:%S') + "\n    Old Perm: " \
+        + str(oct(file.perm)) + "\n    New Perm: " + str(oct(info.st_mode)), file=log, end='\n')
         file_changed = True
     #File Access Time
     if info.st_atime != file.time_acc:
@@ -83,12 +71,12 @@ def check_file(files, directories, index):
         file_changed = True
     #File UID changed
     if info.st_uid != file.user:
-        print("File UID Changed: " + file.name + " | time : " + time.strftime('%H:%M:%S') + "\n    Old UID: "
+        print("File UID Changed: " + file.name + " | time : " + time.strftime('%H:%M:%S') + "\n    Old UID: " \
         + str(file.user) + "\n    New UID: " + str(info.st_uid), file=log, end='\n')
         file_changed = True
     #File GID Changed
     if info.st_gid != file.group:
-        print("File GID Changed : " + file.name + " | time : " + time.strftime('%H:%M:%S') + "\n    Old GID: "
+        print("File GID Changed : " + file.name + " | time : " + time.strftime('%H:%M:%S') + "\n    Old GID: " \
         + str(file.group) + "\n    New GID: " + str(info.st_gid), file=log, end='\n')
         file_changed = True
     #File Hard Links Amount Changed
@@ -105,25 +93,17 @@ def check_file(files, directories, index):
 
 def update_file(files, directories, index):
     file = files[index]
-    if path_exists(file.path):
-        temp = make_file(file.path)
-        files[index] = temp
-        print(temp)
-    else:
-        print("Deleted : " + file.path + " | time : " + time.strftime('%H:%M:%S'), file=log, end='\n')
-        file.name = 'DEL'
+    file_p = file.path
+    temp = make_file(file.path)
+    files[index] = temp
+    print(temp)
 
 
 def update_directory(files, directories, index):
     dir = directories[index]
-    if path_exists(dir.path):
-        temp = make_directory(dir.path)
-        directories[index] = temp
-        print(temp)
-    else:
-        print("Deleted : " + dir.path + " | time : " + time.strftime('%H:%M:%S'), file=log, end='\n')
-        dir.name = 'DEL'
-
+    temp = make_directory(dir.path)
+    directories[index] = temp
+    print(temp)
 
 
 def monitor_main(files, directories):
